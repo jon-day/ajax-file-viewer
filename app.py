e9aa8f29e880d1a772f003f7520087ccb4786a19
unknown <jonmfday@gmail.com>
2019-11-20 19:46:03 -0600

    removed a bunch of test code snippets

--- app.py
+++ app.py
@@ -1,61 +1,23 @@
 from flask import Flask, make_response, flash, request, redirect, render_template, jsonify
 import io
 import csv
-from flask_debugtoolbar import DebugToolbarExtension
+
 from my_special_functions import my_special_func_a, my_special_func_web
-UPLOAD_FOLDER = 'uploads\\'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'csv', 'xlsx', 'xls'}
+ALLOWED_EXTENSIONS = {'csv'}
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.debug = True
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SECRET_KEY'] = 'supersicrit'
-toolbar = DebugToolbarExtension(app)
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('/exportFile', methods=['GET'])
-def index():
-    data = my_special_func_a()
-    if data:
-        fieldnames = data[0].keys()
-        
-        si = io.StringIO()
-        cw = csv.DictWriter(si, fieldnames=fieldnames, extrasaction='ignore')    
-        [cw.writerow(i) for i in data]
-        
-        output = make_response(si.getvalue())
-        output.headers["Content-Disposition"] = "attachment; filename=report.csv"
-        output.headers["Content-type"] = "text/csv"
-        return output
-    else:
-        return "no luck bucko"
-
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            reader = csv.DictReader(io.StringIO(file.read().decode('utf-8')))
-            data = [row for row in reader]
-
-            return jsonify(data)
     return render_template('home.html')
-
 
 @app.route('/uploadajax', methods=['POST'])
 def read_data():
@@ -65,7 +27,7 @@ def read_data():
     fileData = [row for row in reader]
     return render_template("display_data.html", data=fieldnames)
 
-@app.route('/test_route', methods=['POST'])
+@app.route('/get_report', methods=['POST'])
 def test_route():
     firstName = request.form['firstName']
     lastName = request.form['lastName']
--- static/main.js
+++ static/main.js
@@ -14,24 +14,4 @@ $(function() {
             },
         });
     });
-
-    $('#testClick').click(function() {
-        var firstName = $("#firstName :selected").text();
-        var lastName = $("#lastName :selected").text();
-        var form_data = new FormData($('#upload-file')[0]);
-        form_data.append("firstName", firstName);
-        form_data.append("lastName", lastName);
-
-        $.ajax({
-            type: 'POST',
-            url: '/test_route',
-            data: form_data,
-            contentType: false,
-            cache: false,
-            processData: false,
-            success: function(retData) {
-                console.log(retData.url);
-            },
-        });
-    });
 });
